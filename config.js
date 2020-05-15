33e4b715d9bd37b284717d6d1fe3169c066b5842
Cristian <zcri@outlook.it>
2020-05-15 12:23:46 +0200

    Removed package lock and .idea directory.

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
     msgSendMs: 600000,
     msgAutoDeleteMs: 600000,
     staffCheckMs: 60000,
-    guild: "1",
-    channel: "",
-    staffRole: "", //353949968136142848
-    words: []
+    guild: "710769681606246480",
+    channel: "710769852104835073",
+    staffRole: "340465865948397568", //353949968136142848
+    words: ["test1","test2","test3","test4","test5","test6"]
 };
